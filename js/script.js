0be022194275f76a905798ffaeac205637178027
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-06 15:47:51 -0300

    feito menu e responsividade

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const menu = document.querySelector("#menu")
+
+const abrir_svg = document.querySelector(".menu")
+const fechar_svg = document.querySelector(".closemenu")
+
+const navbar = document.querySelector(".navbar")
+
+const fechar = [...document.querySelectorAll(".navegar")]
+
+menu.addEventListener("click",()=>{
+    if(navbar.style.display == "flex"){
+        navbar.style.display = "none"
+        fechar_svg.style.display = "none"
+        abrir_svg.style.display = "block"
+    }else{
+        navbar.style.display = "flex"
+        fechar_svg.style.display = "block"
+        abrir_svg.style.display = "none"
+    }
+})
+
+fechar.map((el)=>{
+    el.addEventListener("click",()=>{
+        navbar.style.display = "none"
+    })
+})
