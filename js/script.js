24ce0ced530946b849cca53c59087e0d93d5623e
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-06 16:10:35 -0300

    correção do menu

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const menu = document.querySelector("#menu")
 const abrir_svg = document.querySelector(".menu")
 const fechar_svg = document.querySelector(".closemenu")
 
-const navbar = document.querySelector(".navbar")
+const navbar = document.querySelector("#NAVBAR")
 
-const fechar = [...document.querySelectorAll(".navegar")]
+const fechar = [...document.querySelectorAll("#NAVBAR")]
 
 menu.addEventListener("click",()=>{
     if(navbar.style.display == "flex"){
@@ -22,5 +22,7 @@ menu.addEventListener("click",()=>{
 fechar.map((el)=>{
     el.addEventListener("click",()=>{
         navbar.style.display = "none"
+        fechar_svg.style.display = "none"
+        abrir_svg.style.display = "block"
     })
 })
